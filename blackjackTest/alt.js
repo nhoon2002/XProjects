88d1a643d44d00f1d5f72de39adf59204bfedf28
LAPTOP-2ALLR6GQ\nhoon <nhoon2002@ucla.edu>
2017-08-06 01:50:00 -0700

    beatuy

--- blackjackTest/alt.js
+++ blackjackTest/alt.js
@@ -1,275 +1,305 @@
 var inquirer = require('inquirer');
 /////////////////////////////////
 
-var userBank = 100;
+var userBank = 100; //Default Buy-In
 var userBet;
+var gameEnded = true;
 
-
-
+//Stack system
 var deck = new Stack();
 var userHand = new Stack();
 var dealerHand = new Stack();
 var burnPile = new Stack();
 
-
 deck.makeDeck(1); //creates 1 deck
 deck.shuffle(1); //shuffles that deck 1 time
 
-
-var promptEnd = false;
+// var promptEnd = false;
 
 /////////////////////////////////////
+startGame();
 
-if (deck.cards.length > 10) {
-  playDeck();
-} else {
-  console.log("Shuffling the deck...");
-  deck.shuffle(1);
-  playDeck();
+// promptHS();
+
+function startGame() {
+    //startGame() signal received.
+
+    //if there are less than 10 cards in the deck remaining AND a game is not in session already...
+    if (deck.cards.length <= 10 && gameEnded) {
+        console.log("There are less than 10 cards remaining. Shuffling the deck...");
+        deck.shuffle(1);
+        playDeck();
+    } else {
+        playDeck();
+    }
+
 }
+function playDeck() {
+    console.log('playDeck initiated!');
+    gameEnded = false;
+    userHand = new Stack();
+    dealHands();
 
-promptHS();
-
-
-
-
+}
 
 function dealHands() {
-  burnPile.addCard(deck.deal());
-  console.log("Burned: " + burnPile.cards.toString());
+    //Initial Deal
+    burnPile.addCard(deck.deal());
+    console.log("Burned so far: " + burnPile.cards.toString());
 
-  userHand.addCard(deck.deal()); //deals top card, then adds the dealt card to user's hand
-  dealerHand.addCard(deck.deal());
-  userHand.addCard(deck.deal());
-  dealerHand.addCard(deck.deal());
-  console.log("User: " + userHand.compute() + "--" + userHand.cards[0].toString() + ' & ' + userHand.cards[1].toString());
-  console.log("Dealer shows a " + dealerHand.cards[0].value +" -- " + dealerHand.cards[0].toString() + ' & FACEDOWN');
-  console.log(deck.cards.length + ' cards remaining in the deck.'); //Shows how many cards are in the deck.
+    userHand.addCard(deck.deal()); //deals top card, then adds the dealt card to user's hand
+    dealerHand.addCard(deck.deal());
+    userHand.addCard(deck.deal());
+    dealerHand.addCard(deck.deal());
+    console.log("User: " + userHand.compute() + "--" + userHand.cards[0].toString() + ' & ' + userHand.cards[1].toString());
+    console.log("Dealer shows a " + dealerHand.cards[0].value + " -- " + dealerHand.cards[0].toString() + ' & FACEDOWN');
+    console.log(deck.cards.length + ' cards remaining in the deck.');
+    promptHS();
 }
 
-function dealerFlip() {
-   console.log("Dealer Flips!");
+function dealerFlip(userScore) {
+    console.log("Dealer Flips!");
+    console.log("Dealer has: <br>" + dealerHand.cards)
+    while (dealerHand.compute() < 17) {
+        dealerHand.addCard(deck.deal());
+    }
+    if (dealerHand.compute() > 21) {
+        console.log("dealer bust, you won");
+        promptRegame();
+    } else if (dealerHand.compute() >= 17) {
+        console.log('dealer has ' + dealerHand.compute());
+        console.log('You have %s', userHand.compute());
+        if (userHand.compute() === dealerHand.compute()) {
+            console.log('Push!!');
+            promptRegame();
+        } else if (userHand.compute() > dealerHand.compute()) {
+            console.log('You won!');
+            promptRegame();
+        } else if (userHand.compute() < dealerHand.compute()) {
+            console.log('You lost!');
+            promptRegame();
+        }
+
+    }
+
+}
+
+function promptRegame() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'playAgainBool',
+            message: 'Would you like to play another hand?',
+            choices: ['Yes', 'No']
+        }
+    ]).then(function(prompt) {
+        if (prompt.playAgainBool == 'Yes') {
+            startGame();
+        } else {
+            console.log('Thanks for playing. Exiting...');
+            return;
+        }
+    });
 }
 
 function promptHS() {
-  var user = userHand.compute();
-  var dealer = dealerHand.compute();
-  var options;
+    var user = userHand.compute();
+    var dealer = dealerHand.compute();
+    var options;
 
-  if ( (userHand.cards.length === 2) && (user < 21) && (userHand.cards[0].value !== userHand.cards[1].value) ) {
+    if ((userHand.cards.length === 2) && (user < 21) && (userHand.cards[0].value !== userHand.cards[1].value)) {
 
-    options = ["Hit", "Stay", "Double", "Surrender"];
+        options = ["Hit", "Stay", "Double", "Surrender"];
 
-  } else if ( (userHand.cards.length === 2) && (userHand.cards[0].value === userHand.cards[1].value) ) {
+    } else if ((userHand.cards.length === 2) && (userHand.cards[0].value === userHand.cards[1].value)) {
 
-    options = ["Hit", "Stay", "Double", "Split", "Surrender"];
-  } else if ( (userHand.cards.length > 2) && (user < 21) ) {
-    options = ["Hit", "Stay"];
-} else if (user >= 21) {
-   console.log("You have " + user);
-   promptEnd = true;
-   return;
+        options = ["Hit", "Stay", "Double", "Split", "Surrender"];
+    } else if ((userHand.cards.length > 2) && (user < 21)) {
+        options = ["Hit", "Stay"];
+
+    }
+
+    userMove(options)
+
 }
+
+function userMove(options) {
+    if (!options) {
+        options = ['Hit', 'Stay'];
+    }
 
     inquirer.prompt([
-      {
-        type: "list",
-        name: "userMove",
-        message: "You have " + userHand.compute() + ". Make your move.",
-        choices: options
-      }
+        {
+            type: "list",
+            name: "userMove",
+            message: "You have " + userHand.compute() + ". Make your move.",
+            choices: options
+        }
     ]).then(function(prompt) {
-      switch (prompt.userMove) {
-        case "Hit":
-        userHand.addCard(deck.deal());
-      //   console.log("You have " + userHand.compute());
-          break;
-        case "Double":
-        userHand.addCard(deck.deal());
-      //   console.log("You have " + userHand.compute());
-        promptEnd = true;
-          break;
-         case "Stay":
-         promptEnd = true;
-          break;
-        default:
-          break;
+        switch (prompt.userMove) {
+            case "Hit":
+                userHand.addCard(deck.deal());
+                computeScore(prompt.userMove);
 
+                break;
+            case "Double":
+                userHand.addCard(deck.deal());
+                computeScore(prompt.userMove);
 
-      }
-      computeScore();
-   });
+                break;
+            case "Stay":
+                computeScore(prompt.userMove);
+                break;
+            default:
+                break;
 
+        }
 
-
-
+    });
 
 }
 
-function computeScore() {
-   // console.log("running..");
-   if (promptEnd === false) {
-      promptHS();
-   } else if (promptEnd === true){
-      dealerFlip();
-   }
-}
+function computeScore(move) {
+    console.log('You have: ' + userHand.compute());
+    if (userHand.compute() < 21 && move == 'Hit') {
+        userMove();
+    } else if (userHand.compute() <= 21 && (move == 'Double' || move == 'Stay')) {
+        dealerFlip();
 
-function playDeck(){
-   promptEnd = false;
-  dealHands();
-  // promptHS();
+    } else if (userHand.compute() === 21) {
+        dealerFlip()
 
-
-  //shuffle
-
-  //burn
-  //deal cycle
-  //examine dealer hand and user hand
-    //if user has blackjack, user win BREAK
-    //else...
-
-  //prompt user hit or stay
-    //if hit, deal 1 more card
-      //if user hand <=21
-        //prompt hit or stay
-      //else if user hand > 21
-        //user loses BREAK
-    //if stay, show dealer card
-      //if (dealer hand) > (user hand),
-        // user loses
-      //else
-        // user wins
-
-
+    } else if (userHand.compute() > 21) {
+        console.log('You Busted!')
+        promptRegame();
+    }
 }
 
 //////////////////////////////////////
 function Card(rank, suit, value) {
 
-  this.rank = rank;
-  this.suit = suit;
-  this.value = value;
+    this.rank = rank;
+    this.suit = suit;
+    this.value = value;
 
-  this.toString   = cardToString;
+    this.toString = cardToString;
 
 }
 
 function cardToString() {
 
-  var rank, suit, value;
+    var rank,
+        suit,
+        value;
 
-  switch (this.rank) {
-    case "A" :
-      rank = "Ace";
-      break;
-    case "2" :
-      rank = "Two";
-      break;
-    case "3" :
-      rank = "Three";
-      break;
-    case "4" :
-      rank = "Four";
-      break;
-    case "5" :
-      rank = "Five";
-      break;
-    case "6" :
-      rank = "Six";
-      break;
-    case "7" :
-      rank = "Seven";
-      break;
-    case "8" :
-      rank = "Eight";
-      break;
-    case "9" :
-      rank = "Nine";
-      break;
-    case "10" :
-      rank = "Ten";
-      break;
-    case "J" :
-      rank = "Jack";
-      break;
-    case "Q" :
-      rank = "Queen";
-      break;
-    case "K" :
-      rank = "King";
-      break;
-    default :
-      rank = null;
-      break;
-  }
+    switch (this.rank) {
+        case "A":
+            rank = "Ace";
+            break;
+        case "2":
+            rank = "Two";
+            break;
+        case "3":
+            rank = "Three";
+            break;
+        case "4":
+            rank = "Four";
+            break;
+        case "5":
+            rank = "Five";
+            break;
+        case "6":
+            rank = "Six";
+            break;
+        case "7":
+            rank = "Seven";
+            break;
+        case "8":
+            rank = "Eight";
+            break;
+        case "9":
+            rank = "Nine";
+            break;
+        case "10":
+            rank = "Ten";
+            break;
+        case "J":
+            rank = "Jack";
+            break;
+        case "Q":
+            rank = "Queen";
+            break;
+        case "K":
+            rank = "King";
+            break;
+        default:
+            rank = null;
+            break;
+    }
 
-  switch (this.suit) {
-      case "C" :
-        suit = "Clubs";
-        break;
-      case "D" :
-        suit = "Diamonds";
-        break;
-      case "H" :
-        suit = "Hearts";
-        break;
-      case "S" :
-        suit = "Spades";
-        break;
-      default :
-        suit = null;
-        break;
+    switch (this.suit) {
+        case "C":
+            suit = "Clubs";
+            break;
+        case "D":
+            suit = "Diamonds";
+            break;
+        case "H":
+            suit = "Hearts";
+            break;
+        case "S":
+            suit = "Spades";
+            break;
+        default:
+            suit = null;
+            break;
     }
 
     if (rank == null || suit == null)
-      return "";
+        return "";
 
     return rank + " of " + suit;
-  }
-
-
+}
 
 function Stack() {
 
-  // Create an empty array of cards.
+    // Create an empty array of cards.
 
-  this.cards = new Array();
+    this.cards = new Array();
 
-  this.makeDeck  = stackMakeDeck;
-  this.shuffle   = stackShuffle;
-  this.deal      = stackDeal;
-  this.draw      = stackDraw;
-  this.addCard   = stackAddCard;
-  this.combine   = stackCombine;
-  this.cardCount = stackCardCount;
-  this.compute = stackCompute;
+    this.makeDeck = stackMakeDeck;
+    this.shuffle = stackShuffle;
+    this.deal = stackDeal;
+    this.draw = stackDraw;
+    this.addCard = stackAddCard;
+    this.combine = stackCombine;
+    this.cardCount = stackCardCount;
+    this.compute = stackCompute;
 }
 
 function stackMakeDeck(n) {
 
-  var ranks = new Array("A", "2", "3", "4", "5", "6", "7", "8", "9",
-                        "10", "J", "Q", "K");
-  var suits = new Array("C", "D", "H", "S");
-  var values = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9,
-                          10, 10, 10, 10);
-  var i, j, k;
-  var m;
+    var ranks = new Array("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
+    var suits = new Array("C", "D", "H", "S");
+    var values = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
+    var i,
+        j,
+        k;
+    var m;
 
-  m = ranks.length * suits.length;
+    m = ranks.length * suits.length;
 
-  // Set array of cards.
+    // Set array of cards.
 
-  this.cards = new Array(n * m);
+    this.cards = new Array(n * m);
 
-  // Fill the array with 'n' packs of cards.
+    // Fill the array with 'n' packs of cards.
 
-  for (i = 0; i < n; i++)
-    for (j = 0; j < suits.length; j++)
-      for (k = 0; k < ranks.length; k++)
-        this.cards[i * m + j * ranks.length + k] =
-          new Card(ranks[k], suits[j], values[k]);
-}
+    for (i = 0; i < n; i++)
+        for (j = 0; j < suits.length; j++)
+            for (k = 0; k < ranks.length; k++)
+                this.cards[i * m + j * ranks.length + k] = new Card(ranks[k], suits[j], values[k]);
+            }
 
 // Shuffling a Stack
 //
@@ -277,62 +307,75 @@ function stackMakeDeck(n) {
 
 function stackShuffle(n) {
 
-  var i, j, k;
-  var temp;
+    var i,
+        j,
+        k;
+    var temp;
 
-  // Shuffle the stack 'n' times.
+    // Shuffle the stack 'n' times.
 
-  for (i = 0; i < n; i++)
-    for (j = 0; j < this.cards.length; j++) {
-      k = Math.floor(Math.random() * this.cards.length);
-      temp = this.cards[j];
-      this.cards[j] = this.cards[k];
-      this.cards[k] = temp;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < this.cards.length; j++) {
+            k = Math.floor(Math.random() * this.cards.length);
+            temp = this.cards[j];
+            this.cards[j] = this.cards[k];
+            this.cards[k] = temp;
+        }
     }
-}
 
 function stackDeal() {
 
-  if (this.cards.length > 0)
-    return this.cards.shift();
-  else
-    return null;
-}
+    if (this.cards.length > 0)
+        return this.cards.shift();
+    else
+        return null;
+    }
 
 function stackCompute() {
-  var total = 0;
-  for (var i = 0; i < this.cards.length; i++) {
-    total += this.cards[i].value;
-  }
-  return total;
+    var total = 0;
+    var aces = 0;
+    for (var i = 0; i < this.cards.length; i++) {
+        if (this.cards[i].value === 1) {
+            aces++;
+            this.cards[i].value === 11
+        }
+        total += this.cards[i].value;
+
+    }
+
+    if (aces === false) {
+        return total;
+    } else if ((aces > 0) && (total > 21)) {
+        return total - 10;
+    }
 }
 
 function stackDraw(n) {
 
-  var card;
+    var card;
 
-  if (n >= 0 && n < this.cards.length) {
-    card = this.cards[n];
-    this.cards.splice(n, 1);
-  }
-  else
-    card = null;
+    if (n >= 0 && n < this.cards.length) {
+        card = this.cards[n];
+        this.cards.splice(n, 1);
+    } else
+        card = null;
 
-  return card;
+    return card;
 }
 
 function stackCardCount() {
 
-  return this.cards.length;
+    return this.cards.length;
 }
 
 function stackAddCard(card) {
 
-  this.cards.push(card);
+    this.cards.push(card);
+    // console.log('Added %s', card);
 }
 
 function stackCombine(stack) {
 
-  this.cards = this.cards.concat(stack.cards);
-  stack.cards = new Array();
+    this.cards = this.cards.concat(stack.cards);
+    stack.cards = new Array();
 }
--- blackjackTest/blackjack_logic.js
+++ blackjackTest/blackjack_logic.js
@@ -6,83 +6,65 @@ var wins_count, losses_count, playertotal, dealertotal, p1, p2, d1, d2 = 0;
 //TODO deal with ACE cases
 var card_full_default = [
 
-   {rank: 'a', value: 1},
-   {rank: '2', value: 2},
-   {rank: '3', value: 3},
-   {rank: '4', value: 4},
-   {rank: '5', value: 5},
-   {rank: '6', value: 6},
-   {rank: '7', value: 7},
-   {rank: '8', value: 8},
-   {rank: '9', value: 9},
-   {rank: '10', value: 10},
-   {rank: 'j', value: 10},
-   {rank: 'q', value: 10},
-   {rank: 'k', value: 10},
-   {rank: 'a', value: 1},
-   {rank: '2', value: 2},
-   {rank: '3', value: 3},
-   {rank: '4', value: 4},
-   {rank: '5', value: 5},
-   {rank: '6', value: 6},
-   {rank: '7', value: 7},
-   {rank: '8', value: 8},
-   {rank: '9', value: 9},
-   {rank: '10', value: 10},
-   {rank: 'j', value: 10},
-   {rank: 'q', value: 10},
-   {rank: 'k', value: 10},
-   {rank: 'a', value: 1},
-   {rank: '2', value: 2},
-   {rank: '3', value: 3},
-   {rank: '4', value: 4},
-   {rank: '5', value: 5},
-   {rank: '6', value: 6},
-   {rank: '7', value: 7},
-   {rank: '8', value: 8},
-   {rank: '9', value: 9},
-   {rank: '10', value: 10},
-   {rank: 'j', value: 10},
-   {rank: 'q', value: 10},
-   {rank: 'k', value: 10},
-   {rank: 'a', value: 1},
-   {rank: '2', value: 2},
-   {rank: '3', value: 3},
-   {rank: '4', value: 4},
-   {rank: '5', value: 5},
-   {rank: '6', value: 6},
-   {rank: '7', value: 7},
-   {rank: '8', value: 8},
-   {rank: '9', value: 9},
-   {rank: '10', value: 10},
-   {rank: 'j', value: 10},
-   {rank: 'q', value: 10},
-   {rank: 'k', value: 10}
+   {rank: 'a', value: 1, suite: 's', index: '1'},
+   {rank: '2', value: 2, suite: 's', index: '2'},
+   {rank: '3', value: 3, suite: 's', index: '3'},
+   {rank: '4', value: 4, suite: 's', index: '4'},
+   {rank: '5', value: 5, suite: 's', index: '5'},
+   {rank: '6', value: 6, suite: 's', index: '6'},
+   {rank: '7', value: 7, suite: 's', index: '7'},
+   {rank: '8', value: 8, suite: 's', index: '8'},
+   {rank: '9', value: 9, suite: 's', index: '9'},
+   {rank: '10', value: 10, suite: 's', index: '10'},
+   {rank: 'j', value: 10, suite: 's', index: '11'},
+   {rank: 'q', value: 10, suite: 's', index: '12'},
+   {rank: 'k', value: 10, suite: 's', index: '13'},
+   {rank: 'a', value: 1, suite: 'c', index: '14'},
+   {rank: '2', value: 2, suite: 'c', index: '15'},
+   {rank: '3', value: 3, suite: 'c', index: '16'},
+   {rank: '4', value: 4, suite: 'c', index: '17'},
+   {rank: '5', value: 5, suite: 'c', index: '18'},
+   {rank: '6', value: 6, suite: 'c', index: '19'},
+   {rank: '7', value: 7, suite: 'c', index: '20'},
+   {rank: '8', value: 8, suite: 'c', index: '21'},
+   {rank: '9', value: 9, suite: 'c', index: '22'},
+   {rank: '10', value: 10, suite: 'c', index: '23'},
+   {rank: 'j', value: 10, suite: 'c', index: '24'},
+   {rank: 'q', value: 10, suite: 'c', index: '25'},
+   {rank: 'k', value: 10, suite: 'c', index: '26'},
+   {rank: 'a', value: 1, suite: 'd', index: '27'},
+   {rank: '2', value: 2, suite: 'd', index: '28'},
+   {rank: '3', value: 3, suite: 'd', index: '29'},
+   {rank: '4', value: 4, suite: 'd', index: '30'},
+   {rank: '5', value: 5, suite: 'd', index: '31'},
+   {rank: '6', value: 6, suite: 'd', index: '32'},
+   {rank: '7', value: 7, suite: 'd', index: '33'},
+   {rank: '8', value: 8, suite: 'd', index: '34'},
+   {rank: '9', value: 9, suite: 'd', index: '35'},
+   {rank: '10', value: 10, suite: 'd', index: '36'},
+   {rank: 'j', value: 10, suite: 'd', index: '37'},
+   {rank: 'q', value: 10, suite: 'd', index: '38'},
+   {rank: 'k', value: 10, suite: 'd', index: '39'},
+   {rank: 'a', value: 1, suite: 'h', index: '40'},
+   {rank: '2', value: 2, suite: 'h', index: '41'},
+   {rank: '3', value: 3, suite: 'h', index: '42'},
+   {rank: '4', value: 4, suite: 'h', index: '43'},
+   {rank: '5', value: 5, suite: 'h', index: '44'},
+   {rank: '6', value: 6, suite: 'h', index: '45'},
+   {rank: '8', value: 8, suite: 'h', index: '46'},
+   {rank: '7', value: 7, suite: 'h', index: '47'},
+   {rank: '9', value: 9, suite: 'h', index: '48'},
+   {rank: '10', value: 10, suite: 'h', index: '49'},
+   {rank: 'j', value: 10, suite: 'h', index: '50'},
+   {rank: 'q', value: 10, suite: 'h', index: '51'},
+   {rank: 'k', value: 10, suite: 'h', index: '52'}
 
 ]
 
-var j=0;
-//adds suite and index to each card
-for (j = 0; j < 13; j++) {
-   card_full_default[j].suite='spade';
-   card_full_default[j].index=j;
-}
-for (j = 13; j < 26; j++) {
-   card_full_default[j].suite='clover';
-   card_full_default[j].index=j;
-}
-for (j = 26; j < 39; j++) {
-   card_full_default[j].suite='diamond';
-   card_full_default[j].index=j;
-}
-for (j = 39; j < 52; j++) {
-   card_full_default[j].suite='heart';
-   card_full_default[j].index=j;
-}
 
 //////////////////////////////////////////////////////////////////
 var newDeck = card_full_default;
-console.log(card_full_default);
+console.log('newDeck: ', card_full_default);
 
 function defaultDeck(array) {
    array = card_full_default;
@@ -92,7 +74,8 @@ function defaultDeck(array) {
 function shuffleDeck(array) {
 
    defaultDeck(array);
-   console.log('Deck has been defaulted.' + array[0].rank);
+   console.log('Deck has been defaulted to stock.'
+   console.log(`Confirm: array[0] = ${array[0].rank} & array[51] = ${array[51].rank}`;
    var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
